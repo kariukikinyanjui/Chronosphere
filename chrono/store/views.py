cd9af46f9a4c6d5b0b39b78df98acf176d26307c
Restyled.io <commits@restyled.io>
2024-05-22 21:50:14 +0000

    Restyled by autopep8

--- chrono/store/views.py
+++ chrono/store/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Product
 
+
 def index(request):
     """
     This function renders the 'index.html' template when the 'home' view is accessed.
     """
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
+
 
 def about(request):
     """
