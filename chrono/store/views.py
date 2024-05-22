081382d386a86e7eb2531247221e24a4081ea63f
Restyled.io <commits@restyled.io>
2024-05-22 21:50:26 +0000

    Restyled by pyment

--- chrono/store/views.py
+++ chrono/store/views.py
@@ -4,15 +4,19 @@ from .models import Product
 
 
 def index(request):
-    """
-    This function renders the 'index.html' template when the 'home' view is accessed.
+    """This function renders the 'index.html' template when the 'home' view is accessed.
+
+    :param request: 
+
     """
     products = Product.objects.all()
     return render(request, "index.html", {"products": products})
 
 
 def about(request):
-    """
-    This function renders the 'about.html' template when the 'about' view is accessed.
+    """This function renders the 'about.html' template when the 'about' view is accessed.
+
+    :param request: 
+
     """
     return render(request, "about.html")
