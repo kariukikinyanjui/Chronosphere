749c57125eea11b17abea57409f8cd6416a8fdeb
Restyled.io <commits@restyled.io>
2024-05-25 17:50:59 +0000

    Restyled by yapf

--- chrono/store/forms.py
+++ chrono/store/forms.py
@@ -8,23 +8,26 @@ class SignUpForm(UserCreationForm):
 
     email = forms.EmailField(
         label="",
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Email Address"}
-        ),
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Email Address"
+        }),
     )
     first_name = forms.CharField(
         label="",
         max_length=100,
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "First Name"}
-        ),
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "First Name"
+        }),
     )
     last_name = forms.CharField(
         label="",
         max_length=100,
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Last Name"}
-        ),
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Last Name"
+        }),
     )
 
     class Meta:
@@ -59,7 +62,8 @@ class SignUpForm(UserCreationForm):
         )
 
         self.fields["password2"].widget.attrs["class"] = "form-control"
-        self.fields["password2"].widget.attrs["placeholder"] = "Confirm Password"
+        self.fields["password2"].widget.attrs[
+            "placeholder"] = "Confirm Password"
         self.fields["password2"].label = ""
         self.fields["password2"].help_text = (
             '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
