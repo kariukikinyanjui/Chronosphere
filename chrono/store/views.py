a61f464ee369ad0d0d5dde4cfc613fbf5f6f0e3e
Kariũki Kĩnyanjui <michaelkariuki7@gmail.com>
2024-05-25 15:41:42 +0300

    [feat]: add login and logout functionality

--- chrono/store/views.py
+++ chrono/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
     """
@@ -13,3 +15,26 @@ def about(request):
     This function renders the 'about.html' template when the 'about' view is accessed.
     """
     return render(request, 'about.html')
+
+def login_user(request):
+    """ A function that logs in the user"""
+    if request.method == 'POST':
+        '''Login user'''
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+            return redirect('index')
+        else:
+            messages.success(request, 'Error logging in')
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+   """ A function that logs out the user"""
+   logout(request)
+   messages.success(request, 'You have been logged out!')
+   return redirect('index')
