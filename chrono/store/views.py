9b244757e811b5c5c7c5f1f5c4cc87769706626c
Restyled.io <commits@restyled.io>
2024-05-25 17:50:56 +0000

    Restyled by black

--- chrono/store/views.py
+++ chrono/store/views.py
@@ -13,60 +13,60 @@ def index(request):
     This function renders the 'index.html' template when the 'home' view is accessed.
     """
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    return render(request, "index.html", {"products": products})
 
 
 def about(request):
     """
     This function renders the 'about.html' template when the 'about' view is accessed.
     """
-    return render(request, 'about.html')
+    return render(request, "about.html")
 
 
 def login_user(request):
-    """ A function that logs in the user"""
-    if request.method == 'POST':
-        '''Login user'''
-        username = request.POST['username']
-        password = request.POST['password']
+    """A function that logs in the user"""
+    if request.method == "POST":
+        """Login user"""
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'You have been logged in!')
-            return redirect('index')
+            messages.success(request, "You have been logged in!")
+            return redirect("index")
         else:
-            messages.success(request, 'Error logging in')
-            return redirect('login')
+            messages.success(request, "Error logging in")
+            return redirect("login")
     else:
-        return render(request, 'login.html', {})
+        return render(request, "login.html", {})
 
 
 def logout_user(request):
-    """ A function that logs out the user"""
+    """A function that logs out the user"""
     logout(request)
-    messages.success(request, 'You have been logged out!')
-    return redirect('index')
+    messages.success(request, "You have been logged out!")
+    return redirect("index")
 
 
 def register_user(request):
-    """ A function that registers the user"""
+    """A function that registers the user"""
     form = SignUpForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         """Register user"""
         form = SignUpForm(request.POST)
         if form.is_valid():
             """Save user"""
             form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request, 'You have successfully registered')
-            return redirect('index')
+            messages.success(request, "You have successfully registered")
+            return redirect("index")
         else:
-            """ Show errors if user not saved"""
-            messages.success(request, 'Error registering user')
-            return redirect('register')
+            """Show errors if user not saved"""
+            messages.success(request, "Error registering user")
+            return redirect("register")
     else:
-        """ Render registration form"""
-        return render(request, 'register.html', {'form': form})
+        """Render registration form"""
+        return render(request, "register.html", {"form": form})
