ae3e243f35130c95827715032c21d691d4edee5f
Gaitho57 <michaelgaitho10@gmail.com>
2024-05-29 02:42:31 +0300

    [feat]: product sale badge

--- chrono/store/models.py
+++ chrono/store/models.py
@@ -37,6 +37,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
+    on_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         """
