ec413a2222adbfe4a2069b51f8384bdd7b887c03
Kariũki Kĩnyanjui <michaelkariuki7@gmail.com>
2024-05-28 15:09:59 +0300

    [feat]: add documentation

--- chrono/cart/cart.py
+++ chrono/cart/cart.py
@@ -8,21 +8,19 @@ class Cart():
         :param request: The HTTP request object
         """
 		self.session = request.session
-		# Acquires the request
 		self.request = request
-		# Gets the current session key if it exists
 		cart = self.session.get('session_key')
 
-		# If new user hence, no session key!  Create one!
+		# Create the cart if it doesn't exist
 		if 'session_key' not in request.session:
 			cart = self.session['session_key'] = {}
-		# Ensure that the cart data is available on all pages of the site
-		self.cart = cart # Holds the current state of the cart
+		self.cart = cart
 
 	def db_add(self, product, quantity):
+		""" Add product to the cart """
 		product_id = str(product)
 		product_qty = str(quantity)
-		# Logic
+
 		if product_id in self.cart:
 			pass
 		else:
@@ -31,51 +29,58 @@ class Cart():
 
 		self.session.modified = True
 
+
 		# Deal with logged in user
 		if self.request.user.is_authenticated:
 			# Get the current user profile
 			current_user = Profile.objects.filter(user__id=self.request.user.id)
 			carty = str(self.cart)
 			carty = carty.replace("\'", "\"")
-			# Save carty to the Profile Model
 			current_user.update(old_cart=str(carty))
 
 
 	def add(self, product, quantity):
+		"""
+		Adds a product to the cart.
+
+		:param product: The product to be added.
+		:type product: Product
+		:param quantity: The quantity of the product to be added.
+		:type quantity: int
+		""" 
 		product_id = str(product.id)
 		product_qty = str(quantity)
-		# Process
+
 		if product_id in self.cart:
-		# Product already exists in the cart
 			pass
 		else:
-			# Add the product to the cart
+			self.cart[product_id] = {'price': str(product.price)}
 			self.cart[product_id] = int(product_qty)
 
 		self.session.modified = True
 
 		# Deal with logged in user
 		if self.request.user.is_authenticated:
-			# Get the current user profile
 			current_user = Profile.objects.filter(user__id=self.request.user.id)
-			# Convert {'3':1, '2':4} to {"3":1, "2":4}
 			carty = str(self.cart)
 			carty = carty.replace("\'", "\"")
-			# Save carty to the Profile Model
 			current_user.update(old_cart=str(carty))
 
 	def cart_total(self):
-		# Aquire product IDS
+		"""
+		Calculate the total cost of the items in the cart.
+
+		Returns:
+		    float: The total cost of the items in the cart.
+
+		"""
 		product_ids = self.cart.keys()
-		# Find the keys in our products database model
 		products = Product.objects.filter(id__in=product_ids)
-		# Get quantities
 		quantities = self.cart
-		# Start counting at zero
 		total = 0
 
 		for key, value in quantities.items():
-			# Convert key that is a string into an integer so we can do math
+			# Convert from string to int
 			key = int(key)
 			for product in products:
 				if product.id == key:
@@ -87,22 +92,47 @@ class Cart():
 
 		#Get the length of the cart and return it
 	def __len__(self):
+		"""
+		Returns the length of the cart.
+
+		Returns:
+		    int: The length of the cart.
+		"""
 		return len(self.cart)
 
 	def get_prods(self):
-		# This retrieves products from cart
+		"""
+		Get the products in the cart.
+
+		Returns:
+		    QuerySet: A queryset of Product objects in the cart.
+		"""
 		product_ids = self.cart.keys()
-		# Uses ids to check for the products in database model
 		products = Product.objects.filter(id__in=product_ids)
 
-		# Returns the retrieved products
 		return products
 
 	def get_quants(self):
+		"""
+		Get the quantities of items in the cart.
+
+		Returns:
+		    dict: A dictionary containing the quantities of items in the cart.
+		"""
 		quantities = self.cart
 		return quantities
 
 	def update(self, product, quantity):
+		"""
+		Updates the quantity of a product in the cart.
+
+		:param product: The product to be updated.
+		:type product: str
+		:param quantity: The new quantity of the product.
+		:type quantity: int
+		:return: The updated cart.
+		:rtype: dict
+		"""
 		product_id = str(product)
 		product_qty = int(quantity)
 
@@ -114,14 +144,11 @@ class Cart():
 		self.session.modified = True
 
 
-		# logged in user
+		# Deal with logged in user
 		if self.request.user.is_authenticated:
-			# Get the current user profile
 			current_user = Profile.objects.filter(user__id=self.request.user.id)
-			# Convert {'3':1, '2':4} to {"3":1, "2":4}
 			carty = str(self.cart)
 			carty = carty.replace("\'", "\"")
-			# Save carty to the Profile Model
 			current_user.update(old_cart=str(carty))
 
 
@@ -129,8 +156,14 @@ class Cart():
 		return thing
 
 	def delete(self, product):
+		"""
+		Deletes a product from the cart.
+
+		:param product: The product to be deleted.
+		:type product: Any
+		""" 
 		product_id = str(product)
-		# Delete from dictionary/cart
+
 		if product_id in self.cart:
 			del self.cart[product_id]
 
@@ -138,10 +171,7 @@ class Cart():
 
 		# logged in user
 		if self.request.user.is_authenticated:
-			# Get the current user profile
 			current_user = Profile.objects.filter(user__id=self.request.user.id)
-			# Convert {'3':1, '2':4} to {"3":1, "2":4}
 			carty = str(self.cart)
 			carty = carty.replace("\'", "\"")
-			# Save carty to the Profile Model
 			current_user.update(old_cart=str(carty))
--- chrono/cart/context_processors.py
+++ chrono/cart/context_processors.py
@@ -1,6 +1,13 @@
 from .cart import Cart
 
-# This context processor ensures our cart can work on all pages of the site
+# Fetches the cart from the session
 def cart(request):
-	# Return the default data from our Cart
+	"""
+	Returns a dictionary containing a single key-value pair. The key is 'cart' and the value is an instance of the Cart class initialized with the given request object.
+
+	:param request: The HTTP request object.
+	:type request: django.http.HttpRequest
+	:return: A dictionary containing a single key-value pair. The key is 'cart' and the value is an instance of the Cart class initialized with the given request object.
+	:rtype: dict
+	"""
 	return {'cart': Cart(request)}
