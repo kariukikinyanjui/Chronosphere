b743b5425f8e5d0cbc2da1464404e009bac2943d
Kariũki Kĩnyanjui <michaelkariuki7@gmail.com>
2024-05-29 12:49:50 +0300

    [feat]: add class Meta

--- chrono/store/models.py
+++ chrono/store/models.py
@@ -5,6 +5,10 @@ import datetime
 class Category(models.Model):
     name = models.CharField(max_length=55)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         """
         Returns a string representation of the object.
