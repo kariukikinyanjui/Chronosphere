8f9fdf9ea25cf3179d733215f4d4e85ee1d9c575
Kariũki Kĩnyanjui <michaelkariuki7@gmail.com>
2024-05-29 22:23:21 +0300

    [feat]: register user functionality

--- chrono/store/views.py
+++ chrono/store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 def index(request):
     """
@@ -66,3 +70,30 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out!')
     return redirect('index')
+
+def register_user(request):
+    """
+    Renders the 'register.html' template and returns the rendered HTML as a response.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response.
+    """
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully registered!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Unable to register your account at this time. Please try again later.')
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
