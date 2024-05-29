535b092131231ef6ad2e79e7f9f9f5458928f019
Kariũki Kĩnyanjui <michaelkariuki7@gmail.com>
2024-05-29 21:07:07 +0300

    [feat]: login/logout functionality

--- chrono/store/views.py
+++ chrono/store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
     """
@@ -25,3 +27,42 @@ def about(request):
         HttpResponse: The rendered HTML response.
     """
     return render(request, 'about.html', {})
+
+def login_user(request):
+    """
+    Renders the 'login.html' template and returns the rendered HTML as a response.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response.
+    """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+            return redirect('index')
+        else:
+            messages.success(request, 'Error logging in. Please try again.')
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    """ 
+    Logs out the user and redirects to the index page.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response.
+    """
+    logout(request)
+    messages.success(request, 'You have been logged out!')
+    return redirect('index')
