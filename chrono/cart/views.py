1c8e58a47a1ad9440c8cbff7041abd70adc207b4
Kariũki Kĩnyanjui <michaelkariuki7@gmail.com>
2024-06-01 21:49:02 +0300

    [feat]: cart messages

--- chrono/cart/views.py
+++ chrono/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 def cart_summary(request):
     """
@@ -31,6 +32,7 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({'qty':product_qty})
+        messages.success(request, 'Cart updated!')
         return response
 
 
@@ -51,6 +53,7 @@ def cart_delete(request):
         cart.delete(product=product_id)
 
         response = JsonResponse({'product':product_id})
+        messages.success(request, 'Product removed from cart!')
         return response 
 
 def cart_add(request):
@@ -72,5 +75,6 @@ def cart_add(request):
 
         cart_quantity = cart.__len__()
 
-        response = JsonResponse({'qty': cart_quantity}) 
+        response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, 'Product added to cart!')
         return response      
