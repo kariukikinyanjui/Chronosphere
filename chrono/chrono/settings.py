9a32a37e5ed182a351155788bde563a09122af40
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-07-17 22:53:53 +0300

    [feat]: updated settings

--- chrono/chrono/settings.py
+++ chrono/chrono/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,7 +77,6 @@ WSGI_APPLICATION = 'chrono.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-load_dotenv()
 
 DATABASES = {
     'default': {
